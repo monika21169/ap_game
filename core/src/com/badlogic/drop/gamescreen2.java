803e8587a7d239801a96ca3d9d2717246db4bbda
monika21169 <monika21169@iiitd.ac.in>
2022-11-21 15:04:22 +0530

    Initial commit

---
+++ core/src/com/badlogic/drop/gamescreen2.java
@@ -0,0 +1,71 @@
+package com.badlogic.drop;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.utils.ScreenUtils;
+
+public class gamescreen2 implements Screen {
+    final Drop game;
+    private Texture backgroundImage;
+    private TextureRegion backgroundTexture;
+    OrthographicCamera camera;
+    public gamescreen2(final Drop game) {
+        this.game = game;
+        backgroundImage = new Texture(Gdx.files.internal("Screen2.png"));
+        backgroundTexture = new TextureRegion(backgroundImage, 0, 0, 800, 480);
+        camera = new OrthographicCamera();
+        camera.setToOrtho(false, 800, 480);
+    }
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void render(float delta) {
+        ScreenUtils.clear(0, 0, 0, 0);
+
+        camera.update();
+        game.batch.setProjectionMatrix(camera.combined);
+
+        game.batch.begin();
+        game.batch.draw(backgroundTexture, 0,0, 800, 480);
+        game.font.draw(game.batch, "Welcome to Tank!", 300, 240);
+        game.font.draw(game.batch, "Click anywhere to begin!", 300, 140);
+
+        game.batch.end();
+        if (Gdx.input.justTouched()) {
+            game.setScreen(new gamescreen3(game));
+            dispose();
+        }
+
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
---
+++ core/src/com/badlogic/drop/gamescreen3.java
@@ -0,0 +1,69 @@
+package com.badlogic.drop;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.utils.ScreenUtils;
+
+public class gamescreen3 implements Screen {
+    final Drop game;
+    private Texture backgroundImage;
+    private TextureRegion backgroundTexture;
+    OrthographicCamera camera;
+    public gamescreen3(final Drop game) {
+        this.game = game;
+        backgroundImage = new Texture(Gdx.files.internal("Screen3.png"));
+        backgroundTexture = new TextureRegion(backgroundImage, 0, 0, 800, 480);
+        camera = new OrthographicCamera();
+        camera.setToOrtho(false, 800, 480);
+    }
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void render(float delta) {
+        ScreenUtils.clear(0, 0, 0, 0);
+
+        camera.update();
+        game.batch.setProjectionMatrix(camera.combined);
+
+        game.batch.begin();
+        game.batch.draw(backgroundTexture, 0,0, 800, 480);
+
+        game.batch.end();
+//
+//        if (Gdx.input.isTouched()) {
+//            game.setScreen(new gamescreen3(game));
+//            dispose();
+//        }
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
